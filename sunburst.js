3a37026230a62f602e39fadec4dbd34ea2d5183d
daniel thomson <daniel.thomson@abc.net.au>
2014-11-04 15:47:12 +1100

    added tooltip option

--- sunburst.js
+++ sunburst.js
@@ -1,6 +1,6 @@
 // extend code
 // https://github.com/dansdom/extend
-var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=arguments.length,j=!1,d={hasOwn:Object.prototype.hasOwnProperty,class2type:{},type:function(a){return null==a?String(a):d.class2type[Object.prototype.toString.call(a)]||"object"},isPlainObject:function(a){if(!a||"object"!==d.type(a)||a.nodeType||d.isWindow(a))return!1;try{if(a.constructor&&!d.hasOwn.call(a,"constructor")&&!d.hasOwn.call(a.constructor.prototype,"isPrototypeOf"))return!1}catch(c){return!1}for(var b in a);return void 0===b||d.hasOwn.call(a, b)},isArray:Array.isArray||function(a){return"array"===d.type(a)},isFunction:function(a){return"function"===d.type(a)},isWindow:function(a){return null!=a&&a==a.window}};"boolean"===typeof c&&(j=c,c=arguments[1]||{},f=2);"object"!==typeof c&&!d.isFunction(c)&&(c={});k===f&&(c=this,--f);for(;f<k;f++)if(null!=(h=arguments[f]))for(g in h)b=c[g],e=h[g],c!==e&&(j&&e&&(d.isPlainObject(e)||(i=d.isArray(e)))?(i?(i=!1,b=b&&d.isArray(b)?b:[]):b=b&&d.isPlainObject(b)?b:{},c[g]=Extend(j,b,e)):void 0!==e&&(c[g]= e));return c};
+var extend = extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=arguments.length,j=!1,d={hasOwn:Object.prototype.hasOwnProperty,class2type:{},type:function(a){return null==a?String(a):d.class2type[Object.prototype.toString.call(a)]||"object"},isPlainObject:function(a){if(!a||"object"!==d.type(a)||a.nodeType||d.isWindow(a))return!1;try{if(a.constructor&&!d.hasOwn.call(a,"constructor")&&!d.hasOwn.call(a.constructor.prototype,"isPrototypeOf"))return!1}catch(c){return!1}for(var b in a);return void 0===b||d.hasOwn.call(a, b)},isArray:Array.isArray||function(a){return"array"===d.type(a)},isFunction:function(a){return"function"===d.type(a)},isWindow:function(a){return null!=a&&a==a.window}};"boolean"===typeof c&&(j=c,c=arguments[1]||{},f=2);"object"!==typeof c&&!d.isFunction(c)&&(c={});k===f&&(c=this,--f);for(;f<k;f++)if(null!=(h=arguments[f]))for(g in h)b=c[g],e=h[g],c!==e&&(j&&e&&(d.isPlainObject(e)||(i=d.isArray(e)))?(i?(i=!1,b=b&&d.isArray(b)?b:[]):b=b&&d.isPlainObject(b)?b:{},c[g]=extend(j,b,e)):void 0!==e&&(c[g]= e));return c};
 
 // D3 plugin template
 (function (d3) {
@@ -8,22 +8,16 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
     'use strict';
     
     // Plugin namespace definition
-    d3.Sunburst = function (options, element, callback)
-    {
+    d3.Sunburst = function (options, element, callback) {
         // wrap the element in the jQuery object
         this.el = element;
-
+        this.callback = callback;
         // this is the namespace for all bound event handlers in the plugin
         this.namespace = "sunburst";
         // extend the settings object with the options, make a 'deep' copy of the object using an empty 'holding' object
         // using the extend code that I ripped out of jQuery
-        this.opts = Extend(true, {}, d3.Sunburst.settings, options);
+        this.opts = extend(true, {}, d3.Sunburst.settings, options);
         this.init();
-        // run the callback function if it is defined
-        if (typeof callback === "function")
-        {
-            callback.call();
-        }
     };
     
     // these are the plugin default settings that will be over-written by user settings
@@ -43,10 +37,20 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             'borderWidth' : 1
         },
         'fontSize' : 12,
-        // defines the data structure of the document
+        // defines the data structure of the chart
         'dataStructure' : {
             'name' : 'label',
             'value' : 'size'
+        },
+        'tooltip' : { // tooltip options
+            'visible' : true,
+            'id' : 'tooltip',
+            'height' : 60,
+            'width' : 200,
+            'offset' : {
+                'x' : 10,
+                'y' : -30
+            }
         },
         'chartName' : null
     };
@@ -86,8 +90,7 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             // if there is a colour range defined for this chart then use the settings. If not, use the inbuild category20 colour range
             if (this.opts.colorRange.length > 0) {
                 container.color = d3.scale.ordinal().range(this.opts.colorRange);
-            }
-            else {
+            } else {
                 container.color = d3.scale.category20c();
             }
 
@@ -95,6 +98,8 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             this.setLayout();
             // set the chart title
             this.setTitle();
+            // add the tooltip
+            this.addTooltip();
 
             container.path = container.chart.datum(container.data).select(".sunburst").selectAll("path")
                 .data(container.partition.nodes);
@@ -106,18 +111,19 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                 .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
                 //.attr("d", container.arc)
                 .attrTween("d", arcTween)
-                .style("stroke", container.opts.elements.borderColor)
-                .style("stroke-width", container.opts.elements.borderWidth)
-                .style("fill", function(d) {
-                    if (d[container.opts.dataStructure.children]) {
-                        return container.color(d.name);
-                    }
-                    else {
-                        return container.color(d.parent.name);
+                .style({
+                    "stroke" : container.opts.elements.borderColor,
+                    "stroke-width" : container.opts.elements.borderWidth,
+                    "fill" : function(d) {
+                        if (d[container.opts.dataStructure.children]) {
+                            return container.color(d.name);
+                        } else {
+                            return container.color(d.parent.name);
+                        }
                     }
                 });
                 
-            var oldValues = container.path.exit()
+            var oldValues = container.path.exit();
             oldValues.remove();
 
             var newValues = container.path.enter();
@@ -125,20 +131,31 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                 .each(stash)
                 .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
                 //.attr("d", container.arc)
-                .style("stroke", container.opts.elements.borderColor)
-                .style("stroke-width", container.opts.elements.borderWidth)
-                .style("fill", function(d) {
-                    if (d[container.opts.dataStructure.children]) {
-                        return container.color(d.name);
-                    }
-                    else {
-                        return container.color(d.parent.name);
-                    }
+                .style({
+                    "stroke" : container.opts.elements.borderColor,
+                    "stroke-width" : container.opts.elements.borderWidth,
+                    "fill" : function(d) {
+                        if (d[container.opts.dataStructure.children]) {
+                            return container.color(d.name);
+                        } else {
+                            return container.color(d.parent.name);
+                        }
+                    },
+                    "fill-rule" : "evenodd"
                 })
-                .style("fill-rule", "evenodd")
                 .transition()
                 .duration(container.opts.speed)
-                .attrTween("d", arcTween);   
+                .attrTween("d", arcTween); 
+
+            if (container.opts.tooltip.visible) {
+                container.addTooltipEvents(container.path);
+                //container.addTooltipEvents(newValues);
+            }
+
+            // run the callback function after the plugin has finished initialising
+            if (typeof container.callback === "function") {
+                container.callback.call(this, container);
+            }  
         },
         setLayout : function() {
             var container = this;
@@ -149,8 +166,10 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                     
             }
             container.chart
-                .attr("width", container.opts.width)
-                .attr("height", container.opts.height);
+                .attr({
+                    "width" : container.opts.width,
+                    "height" : container.opts.height
+                });
 
             // define the sunburst element
             if (!container.sunburst) {
@@ -168,7 +187,7 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             }
             container.partition
                 .size([2 * Math.PI, container.opts.radius * container.opts.radius])
-                .children(function(d) {return d[container.opts.dataStructure.children]})
+                .children(function(d) {return d[container.opts.dataStructure.children]; })
                 .value(function(d) { return d[container.opts.dataStructure.value]; });
 
             // define the arc
@@ -179,6 +198,63 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                     .innerRadius(function(d) { return Math.sqrt(d.y); })
                     .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
             }
+        },
+        addTooltip : function() {
+            var container = this,
+                toolOpts = container.opts.tooltip,
+                tooltip, name, value;
+
+            if (toolOpts.visible) {
+                // create a stacking context
+                d3.select(container.el).style("position", "relative");
+                // if the tooltip already exists then remove it
+                tooltip = d3.select(container.el).select("#" + toolOpts.id).remove();
+                tooltip = d3.select(container.el).append("div");                
+
+                tooltip.attr('id', toolOpts.id)
+                    .attr("class", "tooltip")
+                    .style({
+                        "height" : toolOpts.height + "px",
+                        "width" : toolOpts.width + "px",
+                        "position" : "absolute",
+                        "display" : "none",
+                        "top" : "0px",
+                        "left" : "0px"
+                    });
+                name = tooltip.append("div").attr("class", "name");
+                name.append("label").text("Name: ");
+                name.append("span");
+                value = tooltip.append("div").attr("class", "value");
+                value.append("label").text("Value: ");
+                value.append("span")
+            }
+        },
+        addTooltipEvents : function(elements) {
+            var container = this;
+
+            // remove any previously bound tooltip first
+            elements
+                .on("mouseover.tooltip", null)
+                .on("mouseout.tooltip", null)
+                .on("mouseover.tooltip", function(d, i) {
+                    var tooltip = d3.select("#" + container.opts.tooltip.id),
+                        mouse = d3.mouse(container.el);
+
+                    tooltip.style({
+                        "display" : "block",
+                        "left" : (mouse[0] + container.opts.tooltip.offset.x) + "px",
+                        "top" : (mouse[1] + container.opts.tooltip.offset.y) + "px"
+                    });
+
+                    tooltip.select(".name").select("span")
+                        .text(d.name);
+
+                    tooltip.select(".value").select("span")
+                        .text(d.value);
+                })
+                .on("mouseout.tooltip", function(d, i) {
+                    d3.select("#" + container.opts.tooltip.id).style("display", "none");
+                });
         },
         setTitle : function() {
             var container = this;
@@ -218,9 +294,9 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                     node[children].forEach(function(child) { recurse(node[container.opts.dataStructure.name], child); });
                 }
                 else {
-                    dataList.push({packageName: name, className: node[container.opts.dataStructure.name], value: parseFloat(node[container.opts.dataStructure.value])});
+                    dataList.push({name: name, className: node[container.opts.dataStructure.name], value: parseFloat(node[container.opts.dataStructure.value])});
                 }
-            };
+            }
 
             recurse(null, data);
             return {children: dataList};  
@@ -245,8 +321,7 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             if (container.opts.data) {
                 container.data = container.opts.data;
                 container.updateChart();
-            }
-            else {
+            } else {
                 d3.json(this.opts.dataUrl, function(error, data) {
                     // data object
                     container.data = data;
@@ -257,13 +332,13 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
         },
         // updates the settings of the chart
         settings : function(settings) {
-            // the data object is giving to much recursion on the Extend function.
+            // the data object is giving to much recursion on the extend function.
             // will have to manually clean it if more data is being set
             if (settings.data) {
                 this.opts.data = null;
             }
             // I need to sort out whether I want to refresh the graph when the settings are changed
-            this.opts = Extend(true, {}, this.opts, settings);
+            this.opts = extend(true, {}, this.opts, settings);
             // will make custom function to handle setting changes
             this.getData();
         },
@@ -272,6 +347,9 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             this.el.removeAttribute(this.namespace);
             this.el.removeChild(this.el.children[0]);
             this.el[this.namespace] = null;
+            if (this.opts.tooltip.visible) {
+                d3.select(this.el).select("#" + this.opts.tooltip.id).remove();
+            }
         }     
     };
     
@@ -280,7 +358,8 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
     d3.sunburst = function(element, options, callback) {
         // define the plugin name here so I don't have to change it anywhere else. This name refers to the jQuery data object that will store the plugin data
         var pluginName = "sunburst",
-            args;
+            args,
+            i;
 
         function applyPluginMethod(el) {
             var pluginInstance = el[pluginName];   
@@ -298,7 +377,7 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             else {
                 pluginInstance[options].apply(pluginInstance, args);
             }
-        };
+        }
 
         function initialisePlugin(el) {
             // define the data object that is going to be attached to the DOM element that the plugin is being called on
@@ -315,7 +394,7 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                 // I think I need to anchor this new object to the DOM element and bind it
                 el[pluginName] = new d3.Sunburst(options, el, callback);
             }
-        };
+        }
         
         // if the argument is a string representing a plugin method then test which one it is
         if ( typeof options === 'string' ) {
@@ -323,9 +402,9 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             args = Array.prototype.slice.call(arguments, 2);
             // iterate over each object that the function is being called upon
             if (element.length) {
-                for (var i = 0; i < element.length; i++) {
+                for (i = 0; i < element.length; i++) {
                     applyPluginMethod(element[i]);
-                };
+                }
             }
             else {
                 applyPluginMethod(element);
@@ -336,7 +415,7 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
         else {
             // initialise each instance of the plugin
             if (element.length) {
-                for (var i = 0; i < element.length; i++) {
+                for (i = 0; i < element.length; i++) {
                     initialisePlugin(element[i]);
                 }
             }
